0b5ba8f80d6d1d0110d366bf621078523710e097
harshi-glitch <222288649+harshi-glitch@users.noreply.github.com>
2026-02-07 16:50:28 -0800

    add basic to-do functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,55 @@
+import { useState } from "react"
+import "./styles.css"
+
 export default function App() {
+
+  const [todos, setTodos] = useState([])
+  const [newItem, setNewItem] = useState("")
+
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title:newItem,
+        completed: false },
+      ]
+    })
+
+    setNewItem("")
+  }
+
   return(
     <>
+    <form onSubmit={handleSubmit} className="new-item-form">
+      <div className="form-row">
+        <label htmlFor="item">New item</label>
+        <input 
+        value={newItem} 
+        onChange={e =>setNewItem(e.target.value)}
+        type="text" 
+        id="item"/>
+      </div>
+      <button className="btn">Add</button>
+    </form>
+
+    <h1 className="header">Todo List</h1>
+    <ul className="list">
+      {todos.map(todo => {
+        return (
+        <li key={todo.id}>
+        <label>
+          <input type="checkbox" checked = {todo.completed}/>
+          {todo.title}
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+        )
+      })}
+
+    </ul>
     </>
   )
 }
