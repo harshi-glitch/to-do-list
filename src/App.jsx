ea599cff9ccfcc0c7594fd07bc31026b22e93c5b
harshi-glitch <222288649+harshi-glitch@users.noreply.github.com>
2026-02-07 17:31:04 -0800

    Add toggle and delete functionality to todos

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,25 @@ export default function App() {
     setNewItem("")
   }
 
+  function toggleTodo(id, completed) {
+  setTodos(currentTodos => {
+    return currentTodos.map(todo => {
+      if (todo.id === id) {
+        return {...todo, completed}
+      }
+
+      return todo;
+    
+    })
+  })
+}
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
   return(
     <>
     <form onSubmit={handleSubmit} className="new-item-form">
@@ -37,14 +56,20 @@ export default function App() {
 
     <h1 className="header">Todo List</h1>
     <ul className="list">
+      {todos.length === 0 && "No Todos"}
       {todos.map(todo => {
         return (
         <li key={todo.id}>
         <label>
-          <input type="checkbox" checked = {todo.completed}/>
+          <input type="checkbox" checked = {todo.completed}
+            onChange={e => toggleTodo(todo.id, e.target.checked)}/>
           {todo.title}
         </label>
-        <button className="btn btn-danger">Delete</button>
+        <button 
+        onClick={() => deleteTodo(todo.id)}
+        className= "btn btn-danger">
+          Delete
+          </button>
       </li>
         )
       })}
